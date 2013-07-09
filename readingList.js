1494c5dae0bb5fd0823453dd20ff32a2945256ca
Student <user@chromebox-001.(none)>
2013-07-09 14:39:13 -0700

    Finish BookList

--- readingList.js
+++ readingList.js
@@ -1,8 +1,5 @@
-// create a class through the constructor invocation pattern 
-// BookList = function() {}
-
 var BookList= function(){
-	/* properties of BookList
+    /* properties of BookList
 	Number of books marked as read (eg, BookList.booksRead)
 	Number of books marked not read yet
 	Next book to read(book object)(eg, BookList.nextBook() - returns a Book)
@@ -12,27 +9,57 @@ var BookList= function(){
 	*/
 
 	this.booksRead = 0;
-	this.bookstoRead = 0;
-	
+
+	this.booksToRead = 0;
+
 	this.nextBook = function(){
-
+        //console.log(this.bookShelf);
+		for(var b in this.bookShelf){
+           //console.log(this.bookShelf[b].read)
+			if(this.bookShelf[b].read === false){
+                var nextBook = this.bookShelf[b];
+                //console.log(nextBook);
+				return nextBook;
+			}
+		}
 	};
 
-	this.currentBook = currentBook;
-	this.lastBookRead = lastBookRead;
+	this.currentBook = function(){
+        var currentBook = this.nextBook();
+        //console.log(currentBook);
+		return currentBook;
+	};
+
+	this.lastBookRead = function(){
+		for(var i= 0; i < this.bookShelf.length; i++){
+			if (i === this.bookShelf.length && this.bookShelf[i] === true){
+				return this.bookShelf[i];
+			}
+			else if (this.bookShelf[i].read === true && this.bookShelf[i+1].read === false) {
+				return this.bookShelf[i];
+			}
+			else{
+				console.log("No book last read.");
+			}
+		}
+	};
+
 	this.bookShelf = [];
-	
 
-	this.add = function(libro){
-		this.bookShelf.push(libro);
-		this.currentBook = libro;
-		this.bookstoRead ++;
+	this.add = function(Book){
+		this.bookShelf.push(Book);
+		this.booksToRead++;
 	};
 
-	this.finishCurrentBook= function(libro){
-		
-	}
-}
+	this.finishCurrentBook= function(){
+		var finishBook = this.currentBook();
+		this.finishBook.read = true;
+		this.finishBook.readDate = new Date(dateString);
+		this.booksRead++;
+		this.booksToRead--;
+		return ;
+	};
+};
 
 
 var Book = function(bookTitle, bookAuthor, bookGenre){
@@ -43,6 +70,14 @@ var Book = function(bookTitle, bookAuthor, bookGenre){
 	this.readDate = null;
 };
 
-var pride = new Book("Pride and Prejudice","Libby", "Historical ROmance");
+var cassieBooks = new BookList();
+
+
+var pride = new Book("Pride and Prejudice","Libby", "Historical Romance");
+var toyStory = new Book("Toy Story", "Disney", "Family");
+
+cassieBooks.add(pride);
+cassieBooks.add(toyStory);
+
 
 
